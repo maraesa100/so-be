4148313acb943bb183b2a39b062398c889a8115b
maraesa100 <siobhanmckenzie90@gmail.com>
2021-02-14 11:56:28 +0000

    PC crash stash

---
+++ test/test.js
@@ -0,0 +1,12 @@
+'use strict'
+const express = require('express')
+
+const expect = require('chai').expect
+const request = require('supertest')
+const app = express()
+
+describe('POST Create User Wallet', () => {
+  it('should create wallet for the user', () => {
+    // code for testing the api
+  })
+})
