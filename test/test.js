cdde1f53dfdf2040d090f7ef6950642a792e32d1
maraesa100 <siobhanmckenzie90@gmail.com>
2021-02-14 21:57:15 +0000

    cleaning up testing config

--- test/test.js
+++ test/test.js
@@ -1,14 +1,20 @@
 const express = require('express')
-const chai = require('chai')
+const expect = require('chai').expect
 const request = require('supertest')
 const app = require('../src/app.ts')
 
-describe('GET /plans', function() {
-  it('responds with json', function(done) {
-    request(app)
-      .get('/api/plans')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200, done)
+const gbPlanData = {
+  id: 1,
+  plan_code: 'gb',
+  plan_name: 'UK',
+  monthly_cost: 10,
+  annual_cost: 50
+}
+
+describe('GET /api/plans/gb', function() {
+  it('returns the gb plan data when passed the "gb" string', async function() {
+    const response = await request(app).get('/api/plans/gb')
+    expect(response.status).to.eql(200)
+    expect(response.body.data).to.eql(gbPlanData)
   })
 })
