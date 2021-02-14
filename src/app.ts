86dfb3b3cb230c59ed3fa1f5b1b35186f01ad5ab
maraesa100 <siobhanmckenzie90@gmail.com>
2021-02-14 11:22:31 +0000

    first commit

---
+++ src/app.ts
@@ -0,0 +1,58 @@
+const express = require('express')
+const helmet = require('helmet')
+
+const app = express()
+var bodyParser = require('body-parser')
+var db1 = require('./database.js')
+
+// middleware
+// app.use(helmet())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+var port = process.env.PORT || 8080
+
+var router = express.Router()
+
+router.get('/', (req, res, next) => {
+  res.json({ message: 'Ok' })
+})
+
+router.get('/plans', (req, res, next) => {
+  var sql = 'select * from stockpedia_plans'
+  var params = []
+  db1.all(sql, params, (err, rows) => {
+    if (err) {
+      res.status(400).json({ error: err.message })
+      return
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    })
+  })
+})
+
+router.get('/plans/:plan_code', (req, res, next) => {
+  var sql = 'select * from stockpedia_plans where plan_code = ?'
+  var params = [req.params.plan_code]
+  db1.get(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message })
+      return
+    }
+    res.json({
+      message: 'success',
+      data: row
+    })
+  })
+})
+
+router.use(function(req, res) {
+  res.status(404)
+})
+
+app.use('/api', router)
+
+app.listen(port)
+console.log('Stockopedia magic happening on port ' + port)
---
+++ src/database.ts
@@ -0,0 +1,39 @@
+var sqlite3 = require('sqlite3').verbose()
+// var md5 = require('md5') don't need this import at the moment to hash inputs in the database
+
+const DBSOURCE = 'db.sqlite'
+
+let db = new sqlite3.Database(DBSOURCE, err => {
+  if (err) {
+    // Cannot open database
+    console.error(err.message)
+    throw err
+  } else {
+    console.log('Connected to the SQLite database.')
+    db.run(
+      `CREATE TABLE stockpedia_plans (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            plan_code text UNIQUE, 
+            plan_name text UNIQUE, 
+            monthly_cost integer,
+            annual_cost integer
+                        )`,
+      err => {
+        if (err) {
+          // Table already created
+        } else {
+          // Table just created, creating some rows
+          var insert =
+            'INSERT INTO stockpedia_plans (plan_code, plan_name, monthly_cost, annual_cost) VALUES (?,?,?,?)'
+          db.run(insert, ['gb', 'UK', 10, 50])
+          db.run(insert, ['fr', 'France', 10, 60])
+          db.run(insert, ['de', 'Germany', 15, 75])
+          db.run(insert, ['us', 'USA', 25, 150])
+          db.run(insert, ['jp', 'Japan', 15, 65])
+        }
+      }
+    )
+  }
+})
+
+module.exports = db
